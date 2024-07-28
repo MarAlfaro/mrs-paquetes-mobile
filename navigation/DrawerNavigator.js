d04a4aff4962ae9a637a5e08a66b89cb3a68e86f
Yancy Alfaro <alfaromargarita556@gmail.com>
2024-07-27 23:18:47 -0600

    Menu según usuario

--- navigation/DrawerNavigator.js
+++ navigation/DrawerNavigator.js
@@ -1,12 +1,13 @@
 import React from "react";
-import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList } from "@react-navigation/drawer";
-import { Image, View, StyleSheet, Text } from "react-native";
-import Icon from "react-native-vector-icons/FontAwesome"; // o cualquier otra librería de iconos que prefieras
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { useSelector } from "react-redux";
 import MisPaquetesScreen from "../screens/MisPaquetesScreen";
 import ProfileScreen from "../screens/auth/ProfileScreen";
 import SeguimientoScreen from "../screens/SeguimientoScreen";
 import TabNavigator from "./TabNavigator";
-import RecoverPasswordScreen from "../screens/RecoverPasswordScreen"; // Importa el componente correcto
+import RecoverPasswordScreen from "../screens/RecoverPasswordScreen";
+import Icon from "react-native-vector-icons/FontAwesome"; // o cualquier otra librería de iconos que prefieras
+import CustomDrawerContent from "./CustomDrawerContent";
 
 const Drawer = createDrawerNavigator();
 
@@ -17,27 +18,126 @@ const drawerIcons = {
   Seguimiento: "search",
 };
 
-const CustomDrawerContent = (props) => {
-  return (
-    <DrawerContentScrollView {...props}>
-      <View style={styles.header}>
-        <Image source={require("../assets/Artboard.png")} style={styles.logo} />
-      </View>
-      <DrawerItemList {...props} />
-    </DrawerContentScrollView>
-  );
-};
-
 const DrawerNavigator = () => {
+  const user = useSelector((state) => state.login.user);
+  const role = user.role || "default";
+
+  const getDrawerScreens = () => {
+    switch (role) {
+      case 'admin':
+        return (
+          <>
+            <Drawer.Screen
+              name="Mrs Paquetes"
+              component={TabNavigator}
+              options={{
+                drawerIcon: ({ color, size }) => (
+                  <Icon name={drawerIcons.MrsPaquetes} color={color} size={size} />
+                ),
+              }}
+            />
+            <Drawer.Screen
+              name="Perfil"
+              component={ProfileScreen}
+              options={{
+                drawerIcon: ({ color, size }) => (
+                  <Icon name={drawerIcons.Profile} color={color} size={size} />
+                ),
+              }}
+            />
+            <Drawer.Screen
+              name="Mis Paquetes"
+              component={MisPaquetesScreen}
+              options={{
+                drawerIcon: ({ color, size }) => (
+                  <Icon name={drawerIcons.MisPaquetes} color={color} size={size} />
+                ),
+              }}
+            />
+            <Drawer.Screen
+              name="Seguimiento"
+              component={SeguimientoScreen}
+              options={{
+                drawerIcon: ({ color, size }) => (
+                  <Icon name={drawerIcons.Seguimiento} color={color} size={size} />
+                ),
+              }}
+            />
+          </>
+        );
+      case 'cliente':
+        return (
+          <>
+          <Drawer.Screen
+              name="Mrs Paquetes"
+              component={TabNavigator}
+              options={{
+                drawerIcon: ({ color, size }) => (
+                  <Icon name={drawerIcons.MrsPaquetes} color={color} size={size} />
+                ),
+              }}
+            />
+            <Drawer.Screen
+              name="Mis Paquetes"
+              component={MisPaquetesScreen}
+              options={{
+                drawerIcon: ({ color, size }) => (
+                  <Icon name={drawerIcons.MisPaquetes} color={color} size={size} />
+                ),
+              }}
+            />
+            <Drawer.Screen
+              name="Perfil"
+              component={ProfileScreen}
+              options={{
+                drawerIcon: ({ color, size }) => (
+                  <Icon name={drawerIcons.Profile} color={color} size={size} />
+                ),
+              }}
+            />
+          </>
+        );
+      case 'conductor':
+        return (
+          <>
+          <Drawer.Screen
+              name="Mrs Paquetes"
+              component={TabNavigator}
+              options={{
+                drawerIcon: ({ color, size }) => (
+                  <Icon name={drawerIcons.MrsPaquetes} color={color} size={size} />
+                ),
+              }}
+            />
+            <Drawer.Screen
+              name="Seguimiento"
+              component={SeguimientoScreen}
+              options={{
+                drawerIcon: ({ color, size }) => (
+                  <Icon name={drawerIcons.Seguimiento} color={color} size={size} />
+                ),
+              }}
+            />
+          </>
+        );
+      default:
+        return (
+          <Drawer.Screen
+              name="Mrs Paquetes"
+              component={TabNavigator}
+              options={{
+                drawerIcon: ({ color, size }) => (
+                  <Icon name={drawerIcons.MrsPaquetes} color={color} size={size} />
+                ),
+              }}
+            />
+        );
+    }
+  };
+
   return (
     <Drawer.Navigator
-      drawerContent={CustomDrawerContent}
-      drawerContentOptions={{
-        activeTintColor: "#635bff",
-        inactiveTintColor: "#FFFFFF",
-        itemStyle: { marginVertical: 0 }, // Ajusta el margen vertical a 0 para eliminar cualquier espacio adicional
-        labelStyle: { fontSize: 16 },
-      }}
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
       screenOptions={{
         headerStyle: {
           backgroundColor: "#212734",
@@ -54,62 +154,19 @@ const DrawerNavigator = () => {
         drawerLabelStyle: { fontSize: 16 },
       }}
     >
-      <Drawer.Screen
-        name="Mrs Paquetes"
-        component={TabNavigator}
-        options={{
-          drawerIcon: ({ color, size }) => (
-            <Icon name={drawerIcons.MrsPaquetes} color={color} size={size} />
-          ),
-        }}
-      />
-      <Drawer.Screen
-        name="Perfil"
-        component={ProfileScreen}
-        options={{
-          drawerIcon: ({ color, size }) => (
-            <Icon name={drawerIcons.Profile} color={color} size={size} />
-          ),
-        }}
-      />
-      <Drawer.Screen
-        name="Mis Paquetes"
-        component={MisPaquetesScreen}
-        options={{
-          drawerIcon: ({ color, size }) => (
-            <Icon name={drawerIcons.MisPaquetes} color={color} size={size} />
-          ),
-        }}
-      />
-      <Drawer.Screen
-        name="Seguimiento"
-        component={SeguimientoScreen}
-        options={{
-          drawerIcon: ({ color, size }) => (
-            <Icon name={drawerIcons.Seguimiento} color={color} size={size} />
-          ),
-        }}
-      />
+      {getDrawerScreens()}
       <Drawer.Screen
         name="RecoverPassword"
-        component={RecoverPasswordScreen} // Usa el componente correcto aquí
-        options={{ drawerLabel: () => null }} // Oculta la pantalla del menú de navegación
+        component={RecoverPasswordScreen}
+        options={{
+          drawerLabel: "Cambiar Contraseña",
+          drawerIcon: ({ color, size }) => (
+            <Icon name="key" color={color} size={size} />
+          ),
+        }}
       />
     </Drawer.Navigator>
   );
 };
-
-const styles = StyleSheet.create({
-  header: {
-    backgroundColor: "#212734",
-    height: 150,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  logo: {
-    width: 120,
-    height: 120,
-  },
-});
 
 export default DrawerNavigator;
