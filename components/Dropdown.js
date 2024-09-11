50eb532e620a35b7169f63805cf6e0348b94c789
jherrera245 <245jh17@gmail.com>
2024-09-10 21:00:57 -0600

    Fix dropdown

--- components/Dropdown.js
+++ components/Dropdown.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Modal, Button, TouchableOpacity, Text } from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
 
-const Dropdown = ({ items, placeholder, searchPlaceholder, onValueChange, defaultValue }) => {
+const DropdownModal = ({ items, placeholder, searchPlaceholder, onValueChange, defaultValue }) => {
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState(defaultValue || null);
+  const [modalVisible, setModalVisible] = useState(false);
 
   useEffect(() => {
-    // Actualiza el valor si el valor predeterminado cambia
     if (defaultValue !== value) {
       setValue(defaultValue);
     }
@@ -18,34 +18,94 @@ const Dropdown = ({ items, placeholder, searchPlaceholder, onValueChange, defaul
     if (onValueChange) {
       onValueChange(newValue);
     }
+    setModalVisible(false)
   };
 
+  const selectedText = items.find(item => item.value === value)?.label || placeholder;
+
   return (
-    <View style={{ width: '100%', zIndex: open ? 1000 : 1 }}>
-      <DropDownPicker
-        open={open}
-        value={value}
-        items={items}
-        setOpen={setOpen}
-        setValue={handleValueChange}
-        setItems={() => {}}
-        searchable={true}
-        placeholder={placeholder}
-        searchPlaceholder={searchPlaceholder}
-        style={styles.dropdown}
-        textStyle={styles.text}
-        placeholderStyle={styles.placeholder}
-        dropDownContainerStyle={styles.dropdownContainer}
-        searchTextInputStyle={styles.searchInput}
-        dropDownStyle={styles.dropDownStyle}
-        itemStyle={styles.item}
-        arrowIconStyle={styles.arrowIcon}
-      />
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => setModalVisible(true)} style={styles.openButton}>
+        <Text style={styles.openButtonText}>{selectedText}</Text>
+      </TouchableOpacity>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={styles.modalView}>
+          <DropDownPicker
+            open={open}
+            value={value}
+            items={items}
+            setOpen={setOpen}
+            setValue={handleValueChange}
+            setItems={() => { }}
+            searchable={true}
+            placeholder={placeholder}
+            searchPlaceholder={searchPlaceholder}
+            style={styles.dropdown}
+            textStyle={styles.text}
+            placeholderStyle={styles.placeholder}
+            dropDownContainerStyle={styles.dropdownContainer}
+            searchTextInputStyle={styles.searchInput}
+            dropDownStyle={styles.dropDownStyle}
+            itemStyle={styles.item}
+            arrowIconStyle={styles.arrowIcon}
+          />
+        </View>
+      </Modal>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  openButton: {
+    height: 56,
+    width: '100%',
+    color: '#000000',
+    backgroundColor: '#FFFFFF',
+    borderRadius: 4,
+    marginBottom: 16,
+    elevation: 2,
+    shadowColor: '#000000',
+    shadowOpacity: 0.1,
+    shadowRadius: 3,
+    shadowOffset: { width: 0, height: 2 },
+    $dark: {
+      backgroundColor: '#1E1E1E',
+      color: '#EAEAEA',
+      borderColor: '#333333',
+      borderWidth: 1,
+      elevation: 2,
+      shadowColor: '#000000',
+      shadowOpacity: 0.5,
+      shadowRadius: 6,
+      shadowOffset: { width: 0, height: 4 },
+    }
+  },
+  openButtonText: {
+    paddingVertical: 16,
+    paddingHorizontal: 16,
+    fontSize: 16,
+    color: '#000000',
+    $dark: {
+      color: '#EAEAEA',
+    }
+  },
+  modalView: {
+    flex: 1,
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.9)',
+  },
   dropdown: {
     width: '100%',
     height: 56,
@@ -61,31 +121,14 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16,
     paddingVertical: 8,
     marginBottom: 16,
-    $dark: {
-      backgroundColor: '#1E1E1E',
-      color: '#EAEAEA',
-      borderColor: '#333333',
-      borderWidth: 1, 
-      elevation: 2,
-      shadowColor: '#000000',
-      shadowOpacity: 0.5,
-      shadowRadius: 6,
-      shadowOffset: { width: 0, height: 4 },
-    }
   },
   text: {
     fontSize: 16,
     color: '#495057',
-    $dark: {
-      color: '#FFFFFF',
-    }
   },
   placeholder: {
     fontSize: 16,
     color: '#6c757d',
-    $dark: {
-      color: '#FFFFFF',
-    }
   },
   dropdownContainer: {
     backgroundColor: '#ffffff',
@@ -100,23 +143,9 @@ const styles = StyleSheet.create({
     zIndex: 1000,
     maxHeight: 250,
     minHeight: 200,
-    $dark: {
-      backgroundColor: '#1E1E1E',
-      color: '#EAEAEA',
-      borderColor: '#333333',
-      borderWidth: 1,
-      elevation: 2,
-      shadowColor: '#000000',
-      shadowOpacity: 0.5,
-      shadowRadius: 6,
-      shadowOffset: { width: 0, height: 4 },
-    }
   },
   searchInput: {
     color: '#495057',
-    $dark: {
-      color: '#FFFFFF',
-    }
   },
   dropDownStyle: {
     backgroundColor: '#ffffff',
@@ -128,10 +157,7 @@ const styles = StyleSheet.create({
   arrowIcon: {
     width: 20,
     height: 20,
-    $dark: {
-      color: '#FFFFFF',
-    }
   },
 });
 
-export default Dropdown;
+export default DropdownModal;
